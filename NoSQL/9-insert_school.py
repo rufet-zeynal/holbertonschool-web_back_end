9faaa3fddc7557e106e01edad3a43d95fe517cf3
rufet-zeynal <rufet.zeynalli2@gmail.com>
2026-01-04 19:40:42 +0000

    nosql

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Inserts a new document in a collection based on kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+    Returns the new _id
+    """
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id
